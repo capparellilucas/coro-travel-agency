d52e52176d8e8ca811df8e6c01a87ce54a3dc713
lucas capparelli <capparelli.lucas@gmail.com>
2023-02-02 22:54:58 -0300

    referrals form and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "referrals_form", to: "pages#referrals_form"
+  resources :referrals, only: [:new, :create, :index, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
