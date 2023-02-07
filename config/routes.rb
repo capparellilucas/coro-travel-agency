3a20abaec6695ae0b64e05885dbd42cd506c5248
lucas capparelli <capparelli.lucas@gmail.com>
2023-02-07 14:01:30 -0300

    starting form backend

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :referrals, only: [:new, :create, :index, :destroy]
+  resources :school_promoter_datum, only: [:new, :create, :index, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ test/controllers/school_promoter_datum_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SchoolPromoterDatumControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
